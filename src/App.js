320e721de1fe7b859fdeb82c78ef729bc5d8f272
Perry Vessels <168404+pvdev@users.noreply.github.com>
2019-09-23 19:46:20 -0400

    added routes with react-router-dom using withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
+
+const HatsPage = props => {
+  console.log(props)
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPage} />
+        <Route component={NoMatch} />
+      </Switch>
+    </div>
+  )
+}
+
+const NoMatch = props => {
+  const { location } = props
+  console.log('No Route: ', props)
+  return (
+    <div>
+      <h3>
+        No match for <code>{location.pathname}</code>
+      </h3>
     </div>
   )
 }
