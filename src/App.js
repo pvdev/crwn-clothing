e272417bcb484e1156d032100f5dbc29761af152
Perry Vessels <168404+pvdev@users.noreply.github.com>
2019-10-01 21:07:52 -0400

    added redux, implemented userReducer, userActions and updated App and Header listener and dispatcher

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
@@ -7,19 +8,14 @@ import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUp from './components/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import Header from './components/header/header.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
+import { setCurrentUser } from './redux/user/user.actions'
 
 class App extends React.Component {
-  constructor() {
-    super()
-
-    this.state = {
-      currentUser: null,
-    }
-  }
-
   unsubscribeFromAuth = null
 
   componentDidMount() {
+    const { setCurrentUser } = this.props
+
     // console.log('Calling componentDidMount')
     // auth.onAuthStateChanged sets up a listener for authn changes
     // we pass the 'next' function and returns a function to unsubscribe user
@@ -29,15 +25,13 @@ class App extends React.Component {
 
         // attach listener with onNext
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data(),
-            },
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
           })
         })
       } else {
-        this.setState({ currentUser: null })
+        setCurrentUser(null)
       }
     })
   }
@@ -50,7 +44,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Header currentUser={this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/signin' component={SignInAndSignUp} />
@@ -74,4 +68,11 @@ const NoMatch = props => {
   )
 }
 
-export default App
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user)),
+})
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App)
